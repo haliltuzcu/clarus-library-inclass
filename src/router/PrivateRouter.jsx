679b8d03ac113c4c7d92aa33794a27c79cbc0b27
haliltuzcu <007canberk@gmail.com>
2023-03-07 23:23:09 +0300

    login

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
-  const user = false
-  return user ? <Outlet/> : <Navigate to= "/login"/>    
-  
-}
+  const user = sessionStorage.getItem("user") || false;
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
 
-export default PrivateRouter
+export default PrivateRouter;
