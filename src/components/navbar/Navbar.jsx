31545160532acf3e62937e504fa66a38cb2ac2c7
haliltuzcu <007canberk@gmail.com>
2023-03-07 22:39:27 +0300

    hamburgermenu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Logo, Menu, MenuLink, Nav } from "./Navbar.style";
+import React, { useState } from "react";
+import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
+import { menuIcon } from "../../helper/iconData";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <Nav justify="space-between">
-      <Logo to="/">OGH Library</Logo>
-      <Menu>
+    <Nav justify="space-between" wrap="wrap">
+      <Logo to="/">Clarus Library</Logo>
+      <HamburgerIcon onClick={() => setShowMenu(!showMenu)}>
+        {menuIcon}
+      </HamburgerIcon>
+      <Menu showMenu={showMenu}>
         <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink to="/register">Register</MenuLink>
+        <MenuLink to="/login">Login</MenuLink>
+        <MenuLink to="/login">Logout</MenuLink>
       </Menu>
     </Nav>
   );
